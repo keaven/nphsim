076df73bb91623b657abf1ca4baffc0cb28df329
keaven <temzq@yahoo.com>
2017-12-31 17:17:03 -0500

    Added probability adjusted averaging to estimation in rm.combo.WLRmax

--- R/rm.combo.WLRmax.R
+++ R/rm.combo.WLRmax.R
@@ -16,7 +16,9 @@
 #' Fleming-Harrington weighting sheme to be used
 #' @param adjust.methods methods used to adjust Type I error for testing multiple weighted logrank tests
 #' @param ties.method methods for dealing with tied event times
-#' @param one.sided logical indicating 1-sided (TRUE) vs 2-sided (FALSE) testing; as currently written, the 
+#' @param one.sided logical indicating 1-sided (TRUE) vs 2-sided (FALSE) testing
+#' @param HT.est logical used when adjust.methods is "asymp", max is TRUE to create an estimate
+#' of HR that is averaged across the specified tests 
 #' @param max indicator of extent of output (TRUE for maximum output)
 #' @param alpha level for testing 
 #' @return The function returns a list with the follow components
@@ -34,9 +36,9 @@
 #'
 #' data(Ex1delayedEffect)
 #' with(Ex1delayedEffect,rm.combo.WLRmax(time=month,status=evntd,arm=trt,adjust.methods="asymp",
-#'                                    wt=list(a1=c(0,1),a2=c(1,1)))) 
+#'                                    HT.est=TRUE,wt=list(a1=c(0,0),a2=c(0,1),a3=c(1,0),a4=c(1,1)))) 
 #' with(Ex1delayedEffect,rm.combo.WLRmax(time=month,status=evntd,arm=trt,
-#'                                    wt=list(a1=c(0,1),a2=c(1,1)))) 
+#'                                    wt=list(a1=c(0,0),a2=c(0,1),a3=c(1,0),a4=c(1,1)))) 
 #'                                    
 #' @import survival survMisc
 #'
@@ -46,28 +48,31 @@
 #' @export
 #' 
 rm.combo.WLRmax<- function(time        = NULL,
-                          status      = NULL,
-                          arm         = NULL,
-                          wt          = NULL,
-                          adjust.methods = c("holm", "hochberg", "hommel", "bonferroni","asymp")[3],
-                          ties.method = c("exact", "breslow", "efron")[2],
-                          one.sided   = FALSE,
-                          max         = TRUE,
-                          alpha       = 0.025
+                           status      = NULL,
+                           arm         = NULL,
+                           wt          = NULL,
+                           adjust.methods = c("holm", "hochberg", "hommel", "bonferroni","asymp")[3],
+                           ties.method = c("exact", "breslow", "efron")[2],
+                           one.sided   = FALSE,
+                           HT.est      = FALSE,
+                           max         = TRUE,
+                           alpha       = 0.025
 )
-{ 
+{
   data.anal <- data.frame(cbind(time,status,arm))
   fit<- survMisc::ten(survival::survfit(survival::Surv(time, status) ~ arm, data = data.anal))  
   
+  
   #Testing
   
-  comp(fit, p= sapply(wt, function(x){x[1]}), q= sapply(wt, function(x){x[2]}))
+  #survMisc::comp(fit, p= sapply(wt, function(x){x[1]}), q= sapply(wt, function(x){x[2]}))
   
   tst.rslt <- attr(fit, 'lrt')
   
   #Combination test (exact form)
   
   if(max & adjust.methods != "asymp"){
+    
     tst.rslt1 <- rbind(tst.rslt[1,],subset(tst.rslt, grepl("FH", tst.rslt$W)))
     Z.tst.rslt1 <- tst.rslt1$Z
     if(one.sided){p.unadjusted <- pnorm(q=tst.rslt1$Z)}
@@ -76,6 +81,7 @@ rm.combo.WLRmax<- function(time        = NULL,
     pval.adjusted <- p.adjust(p.unadjusted, method = adjust.methods)
     pval <- min(pval.adjusted)
     max.index <- which(p.unadjusted == min(p.unadjusted), arr.ind = TRUE)
+    
   }
   
   
@@ -97,8 +103,11 @@ rm.combo.WLRmax<- function(time        = NULL,
     
     combo.wt.list.up <- lapply(combo.wt.list,function(a){mapply('+',a)})
     
+    
+    
     wt2 <- lapply(combo.wt.list.up, function(a){apply(a,1,'sum')/2})
     d1 <- data.frame(do.call(rbind,wt2))
+    
     
     wt3 <- unique(wt2)
     d2 <- data.frame(do.call(rbind,wt3))
@@ -107,7 +116,7 @@ rm.combo.WLRmax<- function(time        = NULL,
     
     #Testing (for calculating the covariances)
     
-    comp(fit2, p= sapply(wt3, function(x){x[1]}), q= sapply(wt3, function(x){x[2]}))
+    #survMisc::comp(fit2, p= sapply(wt3, function(x){x[1]}), q= sapply(wt3, function(x){x[2]}))
     
     tst.rsltt <- attr(fit2, 'lrt')
     tst.rslt2 <- subset(tst.rsltt, grepl("FH", tst.rsltt$W))
@@ -115,11 +124,13 @@ rm.combo.WLRmax<- function(time        = NULL,
     cov.tst.rslt11 <- tst.rslt2$Var
     d2$V <- cov.tst.rslt11
     
+    
     d1d2 <- dplyr::full_join(d1,d2, by = c("X1","X2"))
     
     cov.tst.rslt1 <- d1d2$V
     
     cov.tst.1 <- matrix(NA, nrow=length(wt1), ncol=length(wt1))
+    
     
     cov.tst.1[lower.tri(cov.tst.1, diag=FALSE)] <- cov.tst.rslt1
     cov.tst <- t(cov.tst.1)
@@ -127,43 +138,39 @@ rm.combo.WLRmax<- function(time        = NULL,
     
     diag(cov.tst) <- var.tst.rslt1
     cov.tst.1 <- matrix(Matrix::nearPD(cov.tst)$mat, length(Z.tst.rslt1),length(Z.tst.rslt1))
-    #print(cov.tst.1)
-    
-    #z.val <- as.vector(ginv(Re(sqrtm(cov.tst)))%*%q.tst.rslt1)
     
     z.max <- max(abs(tst.rslt1$Z))
     cor.tst <- cov2cor(cov.tst.1)
-    #print(cor.tst)
     
-    #p.value=P(min(Z) < min(z.val))= 1 - P(Z_i >= min(z.val); for all i)
-    
-    if(one.sided){pval2 <-1 - mvtnorm::pmvnorm(lower = rep(-z.max, length(Z.tst.rslt1)), 
-                                               upper= rep(z.max, length(Z.tst.rslt1)), 
-                                               corr= cor.tst, algorithm= mvtnorm::GenzBretz())[1]
+    if(one.sided){pval2 <-1 - pmvnorm(lower = rep(-z.max, length(Z.tst.rslt1)), 
+                                      upper= rep(z.max, length(Z.tst.rslt1)), 
+                                      corr= cor.tst, algorithm= GenzBretz())[1]
     
     max.tst <- which(abs(Z.tst.rslt1) == max(abs(Z.tst.rslt1)), arr.ind = TRUE) 
+    
     if(Z.tst.rslt1[max.tst] >= 0){pval <- 1 - pval2/2}  
     if(Z.tst.rslt1[max.tst] < 0){pval <- pval2/2}  
+    
     }
     
-    if(!one.sided){pval <- 1 - mvtnorm::pmvnorm(lower = rep(-z.max, length(Z.tst.rslt1)), 
-                                                upper= rep(z.max, length(Z.tst.rslt1)), 
-                                                corr= cor.tst, 
-                                                algorithm= mvtnorm::GenzBretz())[1]}
+    if(!one.sided){pval <- 1 - pmvnorm(lower = rep(-z.max, length(Z.tst.rslt1)), 
+                                       upper= rep(z.max, length(Z.tst.rslt1)), 
+                                       corr= cor.tst, algorithm= GenzBretz())[1]}
     
     p.unadjusted <- pnorm(q=tst.rslt1$Z)
     max.index <- which(p.unadjusted == min(p.unadjusted), arr.ind = TRUE)
+    
   }
-  
-  
   
   #Weighted log-rank test (FH weight): only one weight 
   
   if(!max){
+    
     tst.rslt1 <- subset(tst.rslt, grepl("FH", tst.rslt$W))
     pval <- pnorm(q=tst.rslt1$Z)
     p.unadjusted <- pval
     max.index <- NULL
+    
   }
   
   #Estimation (average HR)
@@ -171,9 +178,11 @@ rm.combo.WLRmax<- function(time        = NULL,
   wt.rslt <- data.frame(attr(fit, 'lrw'))
   
   if(max){
+    
     col.wt <- which(grepl("FH", colnames(wt.rslt))==TRUE, arr.ind = T)
     wt.rslt1.1 <- wt.rslt[,c(1,col.wt)]
     wt.rslt1 <- wt.rslt1.1[,max.index]
+    
   }
   
   if(!max){
@@ -206,21 +215,7 @@ rm.combo.WLRmax<- function(time        = NULL,
   data.anal.w$wt[data.anal.w$wt==0] <- 0.000001
   data.anal.w$wt2 <- -log(data.anal.w$wt)
   
-  #if(!is.null(strata)){
-  
-  #  data.anal.w <- cbind(data.anal.w, Strara.mat)
-  #  ncol.data.anal.w <- ncol(data.anal.w)
-  
-  #  data.anal.w <<- data.anal.w
-  #  ncol.data.anal.w <<- ncol.data.anal.w
-  
-  #   FH.est <- coxph(survival::Surv(time, status) ~ arm + strata(data.anal.w[5:ncol.data.anal.w])+ offset(wt2), weight=wt, method= ties.method, data = data.anal.w)
-  # }
-  
-  #if(is.null(strata)){
-  
   FH.est <- coxph(survival::Surv(time, status) ~ arm + offset(wt2), weight=wt,  method= ties.method, data = data.anal.w)
-  # }
   
   hr <- summary(FH.est)
   hr.est <- hr$conf.int[1]
@@ -230,11 +225,78 @@ rm.combo.WLRmax<- function(time        = NULL,
   if(max){
     hr.low.adjusted <- exp(log(hr.est) - (qnorm(1- (alpha/(length(wt) + 1))))*hr$coefficients[3]) 
     hr.up.adjusted <-  exp(log(hr.est) + (qnorm(1- (alpha/(length(wt) + 1))))*hr$coefficients[3]) 
+    
+  }
+  
+  hr.est.HT <- NULL
+  hr.low.HT <- NULL
+  hr.up.HT <- NULL
+  
+  if(HT.est & max & adjust.methods == "asymp"){
+    
+    nmodel <- length(wt) + 1
+    prob.selection <- NULL
+    hr.est2 <- NULL
+    hr.est2.se <- NULL
+    all.index <- 1:nmodel
+    means <- rep(0,length(all.index))
+    
+    for(i in 1:nmodel){
+      
+      #Probability of model i is selected
+      all.cindex <- all.index[-i]
+      B <- matrix(rep(0,nmodel*nmodel),nrow=nmodel) 
+      diag(B) <- rep(1,nmodel) 
+      B[,i] <- -1 
+      B <- B[-i,]
+      Bmeans <- as.vector(B %*% means)  
+      Bcov <-  B%*%cov.tst.1%*% t(B)
+      Bcor <- cov2cor(Bcov)
+      prob.selection[i] <- pmvnorm(lower=rep(0, length(all.cindex)), upper=Inf, corr=Bcor)
+      
+      wt.rslt2 <- wt.rslt1.1[,i]
+      
+      data.anal.event2 <- subset(data.anal, status==1)
+      data.anal.cens2 <- subset(data.anal, status==0)
+      
+      data.anal.event2 <- data.anal.event2[order(data.anal.event2$time),]
+      
+      #Handing ties and matching length of weights
+      
+      wt.u2 <- unlist(wt.rslt2)
+      
+      time.freq2 <- as.matrix(table(data.anal.event2$time))
+      
+      wt.un2 <- data.frame(cbind(as.numeric(row.names(time.freq2)), time.freq2[,1], wt.u2))
+      wt.all2 <- wt.un2[rep(1:nrow(wt.un2), times=wt.un2[,2]),]
+      data.anal.event2$wt <- wt.all2$wt.u2
+      
+      data.anal.cens2$wt <- rep(1, nrow(data.anal.cens2))
+      
+      data.anal.w2 <- rbind(data.anal.event2, data.anal.cens2)
+      data.anal.w2$wt[data.anal.w2$wt==0] <- 0.000001
+      data.anal.w2$wt2 <- -log(data.anal.w2$wt)
+      
+      FH.est2 <- coxph(survival::Surv(time, status) ~ arm + offset(wt2), weight=wt,  method= ties.method, data = data.anal.w2)
+      
+      hr2 <- summary(FH.est2)
+      hr.est2[i] <- hr2$conf.int[1]
+      hr.est2.se[i] <- hr2$coefficients[3]
+      
+    }
+    
+    prob.selection2 <- prob.selection/sum(prob.selection)
+    hr.est.HT <- exp(as.numeric(prob.selection2%*%log(hr.est2)))
+    hr.est.HT.se <- sqrt(as.numeric(prob.selection2%*%hr.est2.se^2) + as.numeric(prob.selection2%*%log(hr.est2)^2) - (log(hr.est.HT))^2)
+    hr.low.HT <- exp(log(hr.est.HT) - (qnorm(1- alpha)*hr.est.HT.se)) 
+    hr.up.HT <-  exp(log(hr.est.HT) + (qnorm(1- alpha)*hr.est.HT.se)) 
+    
   }
   
   if(max & adjust.methods != "asymp"){out <- list(pval=pval, pval.adjusted = pval.adjusted, p.unadjusted = p.unadjusted, hr.est=hr.est, hr.low=hr.low, hr.up=hr.up, max.index=max.index, hr.low.adjusted= hr.low.adjusted, hr.up.adjusted=hr.up.adjusted,  Z.tst.rslt1= Z.tst.rslt1)}
-  if(max & adjust.methods == "asymp"){out <- list(cor=cor.tst,pval=pval, p.unadjusted= p.unadjusted, hr.est=hr.est, hr.low=hr.low, hr.up=hr.up, max.index=max.index, hr.low.adjusted= hr.low.adjusted, hr.up.adjusted=hr.up.adjusted,  Z.tst.rslt1= Z.tst.rslt1, q.tst.rslt1=q.tst.rslt1, max.abs.z=z.max)}
+  if(HT.est & max & adjust.methods == "asymp"){out <- list(cor=cor.tst,pval=pval, p.unadjusted= p.unadjusted, hr.est=hr.est, hr.low=hr.low, hr.up=hr.up, max.index=max.index, hr.low.adjusted= hr.low.adjusted, hr.up.adjusted=hr.up.adjusted,  Z.tst.rslt1= Z.tst.rslt1, q.tst.rslt1=q.tst.rslt1, max.abs.z=z.max, hr.est.HT=hr.est.HT, hr.low.HT= hr.low.HT, hr.up.HT=hr.up.HT, prob.selection=prob.selection, data.anal.w=data.anal.w)}
+  if(!HT.est & max & adjust.methods == "asymp"){out <- list(cor=cor.tst,pval=pval, p.unadjusted= p.unadjusted, hr.est=hr.est, hr.low=hr.low, hr.up=hr.up, max.index=max.index, hr.low.adjusted= hr.low.adjusted, hr.up.adjusted=hr.up.adjusted,  Z.tst.rslt1= Z.tst.rslt1, q.tst.rslt1=q.tst.rslt1, max.abs.z=z.max, data.anal.w=data.anal.w)}
   if(!max){out <- list(pval=pval, hr.est=hr.est, hr.low=hr.low, hr.up=hr.up)}
   
   return(out)
-}  
+}
